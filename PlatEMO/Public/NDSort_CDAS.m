5501b2e872b6b91dd679a759650a3145e0afb8e4
Cian Steenkamp <ciansteenkamp@gmail.com>
2019-07-05 15:39:23 +0200

    NDSort_CDAS added
    NDSort_CDAS added for CDAS_SMPSO algorithm.

---
+++ PlatEMO/Public/NDSort_CDAS.m
@@ -0,0 +1,43 @@
+function [FrontNo,MaxFNo] = NDSort_CDAS(PopObj,nSort)
+
+% Copyright 2016-2017 Ye Tian
+
+    [N,M] = size(PopObj);
+
+    %% Parameter setting
+    S = 0.39;
+
+    %% Transformation of objective values
+    Norm   = sqrt(sum(PopObj.^2,2));
+    Angle  = acos(1-pdist2(PopObj,eye(M),'cosine'));
+    PopObj = repmat(Norm,1,M).*sin(Angle+S*pi)./sin(S*pi);
+    [PopObj,rank] = sortrows(PopObj);
+    
+    %% Non-dominated sorting
+    FrontNo = inf(1,N);
+    MaxFNo  = 0;
+    while sum(FrontNo<inf) < min(nSort,N)
+        MaxFNo = MaxFNo + 1;
+        for i = 1 : N
+            if FrontNo(i) == inf
+                Dominated = false;
+                for j = i-1 : -1 : 1
+                    if FrontNo(j) == MaxFNo
+                        m = 2;
+                        while m <= M && PopObj(i,m) >= PopObj(j,m)
+                            m = m + 1;
+                        end
+                        Dominated = m > M;
+                        if Dominated || M == 2
+                            break;
+                        end
+                    end
+                end
+                if ~Dominated
+                    FrontNo(i) = MaxFNo;
+                end
+            end
+        end
+    end
+    FrontNo(rank) = FrontNo;
+end
