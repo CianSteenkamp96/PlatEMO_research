a2f72e2563f0b642b93aba87fc5b6c7505747167
Cian Steenkamp <ciansteenkamp@gmail.com>
2019-09-02 15:19:36 +0200

    Add files via upload

---
+++ PlatEMO/data_formatting/formatDataPlatEMO.m
@@ -0,0 +1,23 @@
+% Cian Steenkamp
+% This script will need to be run in each
+% opt_raw_data/algo/instance/problem/objective/file (for KnEA, MOEADD, CDAS_SMPSO) folder
+% and
+% opt_raw_data/algo/problem/objective/file (for NSGAIII) folder.
+% Then move the created files to the appropriate subfolders in the:
+% opt_formatted_data/ folder.
+								
+clc;
+clear;
+
+files = dir('*.mat');
+
+for file = files'
+    d = load(file.name);
+    s = size(d.result, 1);
+    for i = (1 + s): (2 * s)
+        disp(d.result{i}.objs);
+        splitted = split(file.name, '.');
+        fileName = string(strcat(splitted(1, 1), '_iter_', int2str(d.result{i - s})));
+        dlmwrite(fileName, d.result{i}.objs, 'precision', '%.4f', 'delimiter', ' ');
+    end
+end
